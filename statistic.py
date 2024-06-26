ed3485daa9a3dfdd51bc149306b317d2b9fc6880
qibh <qibh@nus.edu.sg>
2024-06-26 12:14:28 +0000

    refine the statistics of in-project apis

--- statistic.py
+++ statistic.py
@@ -436,9 +436,9 @@ class Statistic():
         self._analyze_apis(test_case_no_ref_target_pairs, test_case_rag_ref_target_pairs, api_set)
 
     def _analyze_apis(self, test_case_no_ref_target_pairs, test_case_rag_ref_target_pairs, api_set):
-        target_tc_api_count, no_ref_gen_tc_api_count, no_ref_gen_tc_api_cov_target_tc_api_ratio = self.extract_and_count_apis(test_case_no_ref_target_pairs, api_set)
+        target_tc_api_count, no_ref_gen_tc_api_count, no_ref_gen_tc_api_cov_target_tc_api_ratio, target_tc_api_total, no_ref_gen_tc_api_total = self.extract_and_count_apis(test_case_no_ref_target_pairs, api_set)
 
-        target_tc_api_count, rag_ref_gen_tc_api_count, rag_ref_gen_tc_api_cov_target_tc_api_ratio = self.extract_and_count_apis(test_case_rag_ref_target_pairs, api_set)
+        target_tc_api_count, rag_ref_gen_tc_api_count, rag_ref_gen_tc_api_cov_target_tc_api_ratio, target_tc_api_total, rag_ref_gen_tc_api_total = self.extract_and_count_apis(test_case_rag_ref_target_pairs, api_set)
 
         target_avg_gen_tc_api_count = sum(target_tc_api_count) / len(target_tc_api_count)
 
@@ -448,30 +448,41 @@ class Statistic():
         rag_ref_avg_gen_tc_api_count = sum(rag_ref_gen_tc_api_count) / len(rag_ref_gen_tc_api_count)
         rag_ref_avg_api_cov_ratio = sum(rag_ref_gen_tc_api_cov_target_tc_api_ratio) / len(rag_ref_gen_tc_api_cov_target_tc_api_ratio)
 
-        print(f'[Target] Average API Count: {target_avg_gen_tc_api_count:.2f}')
-        print(f'[no_ref] Average API Count: {no_ref_avg_gen_tc_api_count:.2f}')
-        print(f'[no_ref] Average API Coverage Ratio: {no_ref_avg_api_cov_ratio:.2%}')
-        print(f'[rag_ref] Average API Count: {rag_ref_avg_gen_tc_api_count:.2f}')
-        print(f'[rag_ref] Average API Coverage Ratio: {rag_ref_avg_api_cov_ratio:.2%}')
+        print(f'[Target] Avg #APIs/TC: {target_avg_gen_tc_api_count:.2f}')
+        print(f'[Target] #APIs in Total: {target_tc_api_total}')
+        
+        print(f'[no_ref] Avg #APIs/TC: {no_ref_avg_gen_tc_api_count:.2f}')
+        print(f'[no_ref] Avg API Coverage Ratio per TC: {no_ref_avg_api_cov_ratio:.2%}')
+        print(f'[no_ref] #APIs in Total: {no_ref_gen_tc_api_total}')
+
+        print(f'[rag_ref] Avg #APIs/TC: {rag_ref_avg_gen_tc_api_count:.2f}')
+        print(f'[rag_ref] Avg API Coverage Ratio per TC: {rag_ref_avg_api_cov_ratio:.2%}')
+        print(f'[rag_ref] #APIs in Total: {rag_ref_gen_tc_api_total}')
     
     def extract_and_count_apis(self, test_case_target_pairs, api_set):
         target_tc_api_count = []
+        target_tc_in_project_api_set = set()
         gen_tc_api_count = []
         gen_tc_api_cov_target_tc_api_ratio = []  # the ratio of the number of APIs in the target test case that are also in the generated test case 
+        gen_tc_in_project_api_set = set()
 
         for gen_tc, target_tc, fm_name in test_case_target_pairs:
             fm_name = fm_name.split('::::')[1].split('(')[0]
             target_tc_api = set(extract_method_invocation_from_java_code(target_tc))
             target_tc_api.discard(fm_name)
+
             gen_tc_api = set(extract_method_invocation_from_java_code(gen_tc))
             gen_tc_api.discard(fm_name)
 
             target_tc_in_project_api = target_tc_api & api_set
+            target_tc_in_project_api_set.update(target_tc_in_project_api)
+
             gen_tc_in_project_api = gen_tc_api & api_set
+            gen_tc_in_project_api_set.update(gen_tc_in_project_api)
 
             target_tc_api_count.append(len(target_tc_in_project_api))
             gen_tc_api_count.append(len(gen_tc_in_project_api))
             gen_tc_api_cov_target_tc_api_ratio.append(
                 len(gen_tc_in_project_api & target_tc_in_project_api) / len(target_tc_in_project_api) if len(target_tc_in_project_api) > 0 else 0
                 )
-        return target_tc_api_count, gen_tc_api_count, gen_tc_api_cov_target_tc_api_ratio
+        return target_tc_api_count, gen_tc_api_count, gen_tc_api_cov_target_tc_api_ratio, len(target_tc_in_project_api_set), len(gen_tc_in_project_api_set)
